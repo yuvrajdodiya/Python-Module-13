a1f8207ac823b753a8bda4ae3673ff8309829d3b
yuvrajdodiya <dodiyayuvrajsinhd@gmail.com>
2025-04-07 14:50:05 +0530

    Add files via upload

---
+++ 2.Programming Style/LAB/1.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program that demonstrates the correct use of indentation, comments, and
+variables following PEP 8 guidelines.
+
+"""
+num1 = int(input("Enter the number :"))
+num2 = int(input("Enter the number : "))
+
+sum = num1 + num2
+
+# the addtion of two vairable is store sum vairable 
+
+print(f"{num1} and {num2} addition is {sum}")
---
+++ 3.Core Python Concepts/LAB/3.py
@@ -0,0 +1,6 @@
+# How to create variables in Python?
+name = 'yuvi'
+age = 21
+
+print(name)
+print(age)
